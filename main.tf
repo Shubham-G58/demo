c6dc83e6ef052a7b3e291fcc450517fdd5c65180
Shubham-G58 <shubham.gopchade@apexon.com>
2025-04-27 01:47:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-
-  assume_role_with_web_identity {
-    # Hard-coded IAM Role ARN for GitHub Actions OIDC
-    role_arn = "arn:aws:iam::038462758764:role/github-actions-role" 
-    # Path where GitHub Actions writes the OIDC token
-    web_identity_token_file = "/home/runner/work/_temp/oidc-token" 
-  }
 }
+
 
 resource "aws_instance" "demo" {
   ami           = "ami-0c94855ba95c71c99"
