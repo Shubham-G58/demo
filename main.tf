21ff07d535224c88810213d76fe918abb310a899
Shubham-G58 <shubham.gopchade@apexon.com>
2025-04-29 14:20:13 +0530

    Update main.tf with task 1 changes

--- main.tf
+++ main.tf
@@ -12,11 +12,173 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "demo" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
+#resource "aws_instance" "demo" {
+ # ami           = "ami-0c94855ba95c71c99"
+  #instance_type = "t2.micro"
 
-  tags = {
-    Name = "demo-instance"
+  #tags = {
+   # Name = "demo-instance"
+  #}
+#}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+#######network_componenets#####
+resource "aws_subnet" "public_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.rt.id
+}
+
+############cluster#######
+resource "aws_ecs_cluster" "app_cluster" {
+  name = "my-fargate-cluster"
+}
+######alb #######
+resource "aws_lb" "app_lb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public_a.id]
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+########iam role #########
+resource "aws_iam_role" "ecs_task_exec_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+#######task definiton##########
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "my-app-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_exec_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "my-app"
+    image     = "nginx"  # Or your ECR image
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+############ecs service#####
+resource "aws_ecs_service" "app_service" {
+  name            = "my-app-service"
+  cluster         = aws_ecs_cluster.app_cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id]
+    assign_public_ip = true
+    security_groups = [aws_security_group.ecs_service_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "my-app"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.app_listener]
+}
+##############sg#############
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "alb-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_service_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "ecs-service-sg"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
