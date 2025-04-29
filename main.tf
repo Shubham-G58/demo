e00510b5fd0f413730ae91d4ab801e778ffe8ce7
Shubham-G58 <shubham.gopchade@apexon.com>
2025-04-29 14:28:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_lb" "app_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public_a.id,aws_subnet.public_a.id]
+  subnets            = [aws_subnet.public_a.id,aws_subnet.public_b.id]
 }
 
 resource "aws_lb_target_group" "app_tg" {
